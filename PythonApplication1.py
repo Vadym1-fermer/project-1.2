24a69489827ec362c08b73f6b3309d838cdad15c
Vadym1-fermer <kudeliavadim@gmail.com>
2025-09-02 22:47:23 +0300

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,32 @@
+banana = 1
+apples = 5
+tree_position = (1, 1)
+mood = 'Happy'
+song_title = 'Hello'
+box_size = (10, 12)
+points = 12
+day_range = (1, 12)
+dream = 'Travel'
+stage = 21
+
+print(banana)
+print(apples)
+print(tree_position)
+print(mood)
+print(song_title)
+print(box_size)
+print(points)
+print(day_range)
+print(dream)
+print(stage)
+
+print(type(banana))
+print(type(apples))
+print(type(tree_position))
+print(type(mood))
+print(type(song_title))
+print(type(box_size))
+print(type(points))
+print(type(day_range))
+print(type(dream))
+print(type(stage))
