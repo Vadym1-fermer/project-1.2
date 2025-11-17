6ff7aab4d313b8497b7e6acd75bd35ea7611391e
Vadym1-fermer <kudeliavadim@gmail.com>
2025-11-17 22:07:56 +0200

    Remove unnecessary blank lines in timer_decorator

--- laba6.py
+++ laba6.py
@@ -1,15 +1,13 @@
 import time
 
-
 def timer_decorator(func):
     def wrapper(*args, **kwargs):
         print(f"Викликається функція: {func.__name__}")
         start = time.time()
-
+        
         result = func(*args, **kwargs)
-
+        
         end = time.time()
         print(f"Час виконання: {end - start:.5f} секунд")
         return result
-
     return wrapper
